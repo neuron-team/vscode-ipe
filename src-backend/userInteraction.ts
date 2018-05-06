9b4684cde9e013c94222ced021ae8120a4b84a27
Marco Lavalle <ml7715@ic.ac.uk>
2018-05-06 22:42:29 +0100

    Added jupyter interpreter  info user interface

--- src-backend/userInteraction.ts
+++ src-backend/userInteraction.ts
@@ -8,6 +8,10 @@ export class UserInteraction {
     private _onNewCard: EventEmitter<{title: string, source: string}> = new EventEmitter();
     get onNewCard(): Event<{title: string, source: string}> { return this._onNewCard.event; }
 
+    private _onAskJupyterInfo: EventEmitter<{baseUrl: string, token: string}> = new EventEmitter();
+    get onAskJupyterInfo(): Event<{baseUrl: string, token: string}> { return this._onAskJupyterInfo.event; }
+    
+
     constructor(private context: vscode.ExtensionContext) {
         context.subscriptions.push(vscode.commands.registerCommand('ipe.showWebview', () => {
             this._onShowPane.fire();
@@ -25,5 +29,25 @@ export class UserInteraction {
                 });
             });
         }));
+
+        context.subscriptions.push(vscode.commands.registerCommand('ipe.askJupyterInfo', () => {
+            vscode.window.showInputBox({
+                prompt: 'Provide the base address of the Jupyter notebook',
+                value: 'http://localhost:8888/'
+            }).then(url =>
+                {
+                    vscode.window.showInputBox({
+                        prompt: 'Provide the token to access to Jupyter notebook, leave blank if not used',
+                        value: ''
+                    }).then(givenToken => 
+                        {
+                            this._onAskJupyterInfo.fire({
+                                baseUrl: url,
+                                token: givenToken
+                            });
+                        });
+                })
+        }));
+
     }
 }
