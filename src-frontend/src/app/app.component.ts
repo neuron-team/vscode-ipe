4b6f41182235f4391a686f74b1730a46b8d0ff5e
lorenzo2897 <code@silvestrix.org>
2018-05-07 21:14:24 +0100

    scroll to the bottom of the list when items are added

--- src-frontend/src/app/app.component.ts
+++ src-frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {ExtensionService} from './classes/extension.service';
 import {Card} from "vscode-ipe-types";
 
@@ -7,12 +7,28 @@ import {Card} from "vscode-ipe-types";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  cards: Card[] = [new Card('sample card', 'print("Hello, world!");', 'Hello, world!')];
+export class AppComponent implements AfterViewInit {
+  cards: Card[] = [
+    new Card('sample card', 'print("Hello, world!");', 'Hello, world!')
+  ];
 
   constructor(private extension: ExtensionService) {
     extension.onAddCard.subscribe(card => {
       this.cards.push(card);
     });
+  }
+
+
+  /* this code ensures that the list always scrolls to the bottom when new elements are added */
+  @ViewChildren('listItems') listItems: QueryList<any>;
+  @ViewChild('scrollingList') scrollContainer;
+  ngAfterViewInit() {
+    this.listItems.changes.subscribe(() => this.scrollToBottom());
+    this.scrollToBottom();
+  }
+  scrollToBottom() {
+    try {
+      this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+    } catch(err) { }
   }
 }
