1487bc7babccddc6f3644c90968ce7548373d167
Luke Quinlan <lukeq1000@gmail.com>
2018-05-10 13:28:21 +0100

    Edit Card Title. Connected Collapse to Common.

--- src-frontend/src/app/app.module.ts
+++ src-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import {ExtensionService} from './classes/extension.service';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     ExtensionService
