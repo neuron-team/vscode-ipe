27a7a104324a0dcc87acc1b8d97b23175a78fb88
Marco Lavalle <ml7715@ic.ac.uk>
2018-05-10 19:06:02 +0100

    A kernel for any language can be created (including R), would be useful to link files to the right kernel

--- src-backend/extension.ts
+++ src-backend/extension.ts
@@ -32,7 +32,7 @@ export function activate(context: vscode.ExtensionContext) {
         userInteraction.askJupyterInfo().then(({baseUrl, token}) => {
             // Connect to the server defined
             interpreter.connectToServer(baseUrl, token);
-            // Create a python3 kernel
+            // Create a python3 kernel, any kernel can be chosen
             interpreter.startKernel('python3');
             // Execute code when new card is created
             userInteraction.onNewCard(sourceCode => interpreter.executeCode(sourceCode, 'python3'));
