74e4e12fccbdadec3388fc99c717e97f97199b86
ml7715 <ml7715@ic.ac.uk>
2018-05-12 20:15:15 +0100

    Jupyter is now executed automatically, discovered baseUrl bug

--- src-backend/extension.ts
+++ src-backend/extension.ts
@@ -6,8 +6,10 @@ import {Card} from 'vscode-ipe-types';
 import {WebviewController} from "./webviewController";
 import {Interpreter, ContentHelpers} from "./interpreter";
 import {UserInteraction} from "./userInteraction";
+import {JupyterManager} from './jupyterManager';
 
 export function activate(context: vscode.ExtensionContext) {
+
     let webview: WebviewController = new WebviewController(context);
     let userInteraction: UserInteraction = new UserInteraction(context);
     
@@ -19,20 +21,23 @@ export function activate(context: vscode.ExtensionContext) {
         webview.addCard(card);
     });
 
-    userInteraction.onShowPane(() => {
-        // Initialise status indicator
-        //statusIndicator.text = "Jupyter: idle";
+    function initialisePanel({baseUrl, token}){
+        console.log(baseUrl+" "+ token);
+        // Connect to the server defined
+        interpreter.connectToServer(baseUrl, token);
+        // Create a python3 kernel, any kernel can be chosen
+        interpreter.startKernel('python3');
+        // Execute code when new card is created
+        userInteraction.onNewCard(sourceCode => interpreter.executeCode(sourceCode, 'python3'));
 
-        // Ask info about the interpreter
-        userInteraction.askJupyterInfo().then(({baseUrl, token}) => {
-            // Connect to the server defined
-            interpreter.connectToServer(baseUrl, token);
-            // Create a python3 kernel, any kernel can be chosen
-            interpreter.startKernel('python3');
-            // Execute code when new card is created
-            userInteraction.onNewCard(sourceCode => interpreter.executeCode(sourceCode, 'python3'));
-        });
         webview.show();
+    }
+
+    userInteraction.onShowPane(() => {
+        let jupyterManager = new JupyterManager();
+        jupyterManager.getJupyterInfos()
+            .then(initialisePanel)
+            .catch(() => userInteraction.askJupyterInfo().then(initialisePanel));
     });
 }
 
---
+++ src-backend/jupyterManager.ts
@@ -0,0 +1,40 @@
+import { spawn, ChildProcess } from 'child_process';
+import { URL } from 'url';
+
+export class JupyterManager{
+    private static process: ChildProcess;
+    private static url: URL;
+    private static pattern = /http:\/\/localhost:[0-9]+\/\?token=[a-z0-9]+/g;
+    private static timeout = 3000; // timeout set to 3s
+
+    constructor(){
+        JupyterManager.process = spawn('jupyter', ['notebook', '--no-browser']);
+        JupyterManager.process.stderr.on('data', (data: string) => this.extractJupyterInfos(data));
+    }
+
+    extractJupyterInfos(data: string){
+        let match = JupyterManager.pattern.exec(data);
+        
+        if(match !== null){
+            JupyterManager.url = new URL(match[0]);
+        }
+    }
+
+    public getJupyterInfos(){
+        return new Promise(function(resolve, reject){
+            setTimeout(() => {
+                JupyterManager.process.stderr.removeAllListeners();
+                if(typeof JupyterManager.url === 'undefined'){
+                    reject('Jupyter could not be executed automatically');
+                }
+                else{
+                    resolve(
+                        {
+                            baseUrl: JupyterManager.url.protocol+'//'+JupyterManager.url.hostname+':'+JupyterManager.url.port+'/', 
+                            token: JupyterManager.url.searchParams.get('token')
+                        });
+                }
+            }, JupyterManager.timeout);
+        });
+    }
+}
