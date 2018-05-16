ff8c52c55e161751d7637c56777406c8ffa86370
lorenzo2897 <code@silvestrix.org>
2018-05-17 00:09:20 +0100

    sanitize html tags before passing through highlight pipe, fixes #65

--- src-frontend/src/app/classes/highlight.pipe.ts
+++ src-frontend/src/app/classes/highlight.pipe.ts
@@ -3,16 +3,20 @@ import { PipeTransform, Pipe } from '@angular/core';
 @Pipe({ name: 'highlight' })
 export class HighlightPipe implements PipeTransform {
   transform(text: string, search): string {
-    if (search && text) {
+    let sanitizedText = text
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+    if (search) {
       let pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
       pattern = pattern.split(' ').filter((t) => {
         return t.length > 0;
       }).join('|');
       const regex = new RegExp(pattern, 'gi');
 
-      return text.replace(regex, (match) => `<span class="search-highlight">${match}</span>`);
+      return sanitizedText.replace(regex, (match) => `<span class="search-highlight">${match}</span>`);
     } else {
-      return text;
+      return sanitizedText;
     }
   }
 }
