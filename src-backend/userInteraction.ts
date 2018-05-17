0a33d476c582d37798abde1b5cef4f7e07b7e638
lorenzo2897 <code@silvestrix.org>
2018-05-17 12:26:17 +0100

    trimmed leading and trailing blank lines from code, fixes #46

--- src-backend/userInteraction.ts
+++ src-backend/userInteraction.ts
@@ -13,36 +13,44 @@ export class UserInteraction {
 
     constructor(private context: vscode.ExtensionContext) {
 
-        
         context.subscriptions.push(vscode.commands.registerCommand('ipe.showWebview', () => {
-            this._onShowPane.fire();
+            this.showWebview();
         }));
 
         context.subscriptions.push(vscode.commands.registerCommand('ipe.newCard', () => {
-            if (!vscode.window.activeTextEditor) {
-                vscode.window.showErrorMessage("No file open");
-                return;
-            }
-            if (vscode.window.activeTextEditor.selection.isEmpty) {
-                // Sends current line to the interpreter
-                let lineNr = vscode.window.activeTextEditor.selection.start.line;
-                let sourceCode = vscode.window.activeTextEditor.document.lineAt(lineNr).text;
-                this._onNewCard.fire(sourceCode);
-
-                // Advance text cursor to the next line
-                let newPos = new vscode.Position(lineNr+1, 0);
-                let newSelection = new vscode.Selection(newPos, newPos);
-                vscode.window.activeTextEditor.selection = newSelection;
-            } else {
-                // Send selection to interpreter
-                let sourceCode = vscode.window.activeTextEditor.document.getText(vscode.window.activeTextEditor.selection);
-                this._onNewCard.fire(sourceCode);
-            }
-            
+           this.newCard();
         }));
 
         this.statusIndicator = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
         this.statusIndicator.show();
+    }
+
+    private showWebview() {
+        this._onShowPane.fire();
+    }
+
+    private newCard() {
+        let trimNewlines = s => s.replace(/^(\r?\n)+|(\r?\n)+$/g,'');
+
+        if (!vscode.window.activeTextEditor) {
+            vscode.window.showErrorMessage("No file open");
+            return;
+        }
+        if (vscode.window.activeTextEditor.selection.isEmpty) {
+            // Sends current line to the interpreter
+            let lineNr = vscode.window.activeTextEditor.selection.start.line;
+            let sourceCode = vscode.window.activeTextEditor.document.lineAt(lineNr).text;
+            this._onNewCard.fire(trimNewlines(sourceCode));
+
+            // Advance text cursor to the next line
+            let newPos = new vscode.Position(lineNr+1, 0);
+            let newSelection = new vscode.Selection(newPos, newPos);
+            vscode.window.activeTextEditor.selection = newSelection;
+        } else {
+            // Send selection to interpreter
+            let sourceCode = vscode.window.activeTextEditor.document.getText(vscode.window.activeTextEditor.selection);
+            this._onNewCard.fire(trimNewlines(sourceCode));
+        }
     }
 
     updateStatus(status: string){
