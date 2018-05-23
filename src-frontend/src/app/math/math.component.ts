d436b90966bf76c56a6a4f11ebf28f06c5db3d13
ml7715 <ml7715@ic.ac.uk>
2018-05-23 18:22:03 +0100

    Reduced math component

--- src-frontend/src/app/math/math.component.ts
+++ src-frontend/src/app/math/math.component.ts
@@ -2,9 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-math',
-  template: `<ng-katex [equation]="equation"></ng-katex>`
+  template: `<ng-katex [equation]="mathExpr"></ng-katex>`
 })
 export class MathComponent {
   @Input() mathExpr: string;
-  equation = '\\sum_{i=1}^nx_i';
 }
