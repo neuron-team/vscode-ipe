6912f15815a0bec20dddd6d0a52d90b0e24c4648
ml7715 <ml7715@ic.ac.uk>
2018-05-23 23:28:50 +0100

    ng-katex now renders properly, fixed rendering of math expressions with dollar sign

--- src-frontend/src/app/math/math.component.ts
+++ src-frontend/src/app/math/math.component.ts
@@ -1,9 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { KatexOptions } from 'ng-katex';
 
 @Component({
   selector: 'app-math',
-  template: `<ng-katex [equation]="mathExpr"></ng-katex>`
+  template: `<ng-katex [equation]="mathExprNorm" [options]="options"></ng-katex>`
 })
-export class MathComponent {
+export class MathComponent implements OnInit {
   @Input() mathExpr: string;
+  mathExprNorm: string;
+
+  constructor(){}
+
+  options: KatexOptions = {
+    displayMode: true,
+  };
+
+  ngOnInit(){
+    this.mathExprNorm = this.mathExpr.replace(/\$/g, '');
+  }
 }
