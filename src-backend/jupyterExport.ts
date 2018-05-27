72622ca2b510a67ab641d2d26c5346e6a0ca186c
ml7715 <ml7715@ic.ac.uk>
2018-05-27 13:28:36 +0100

    Fixed export to file for multiple kernels

--- src-backend/jupyterExport.ts
+++ src-backend/jupyterExport.ts
@@ -4,11 +4,48 @@ import * as path from "path";
 
 import * as vscode from 'vscode';
 import { Event, EventEmitter } from "vscode";
+import { JSONObject, JSONArray } from '@phosphor/coreutils';
 
 export class JupyterExport{
     private _onExportToJupyter : EventEmitter<void> = new EventEmitter();
     get onExportToJupyter(): Event<void> { return this._onExportToJupyter.event; }
     
+    private metadataPy = {
+        "kernelspec": {
+            "display_name": "Python 3",
+            "language": "python",
+            "name": "python3"
+        },
+        "language_info": {
+            "codemirror_mode": {
+                "name": "ipython",
+                "version": 3
+            },
+            "file_extension": ".py",
+            "mimetype": "text/x-python",
+            "name": "python",
+            "nbconvert_exporter": "python",
+            "pygments_lexer": "ipython3",
+            "version": "3.6.4"
+        }
+    };
+
+    private metadataR = {
+        "kernelspec": {
+            "display_name": "R",
+            "language": "R",
+            "name": "ir"
+        },
+        "language_info": {
+            "codemirror_mode": "r",
+            "file_extension": ".r",
+            "mimetype": "text/x-r-source",
+            "name": "R",
+            "pygments_lexer": "r",
+            "version": "3.5.0"
+        }
+    };
+
     cardsExecuted: Array<Card> = [];
 
     constructor(private context: vscode.ExtensionContext) {
@@ -21,79 +58,53 @@ export class JupyterExport{
         this.cardsExecuted.push(card);
     }
 
-    private exportToJupyter(){
+    private writeToFile(jupyterFileData: JSONObject, kernelName: string){
         let fullPath = vscode.window.activeTextEditor.document.uri.path;
-        let fileName = fullPath.replace(new RegExp(path.extname(fullPath)+'$'), '.ipynb').slice(1);
-        let extName = path.extname(fullPath);
+        let fileName = fullPath.replace(new RegExp(path.extname(fullPath)+'$'), kernelName+'.ipynb').slice(1);
 
-        let jupyterFileData = {
+        if(jupyterFileData['cells'] != []){
+            fs.writeFile(fileName, JSON.stringify(jupyterFileData), err => {
+                if (err) console.log(err);
+                vscode.window.showInformationMessage(`Saved as: ${fileName}`);
+                // Also, pop out information box to show that it has been saved
+            });
+        }
+    }
+
+    private exportToJupyter(){
+        let jupyterFileDataPython: JSONObject, jupyterFileDataR: JSONObject;
+        jupyterFileDataPython = {
             "nbformat": 4,
             "nbformat_minor": 2
         };
-
-        let metadataPy = {
-            "kernelspec": {
-                "display_name": "Python 3",
-                "language": "python",
-                "name": "python3"
-            },
-            "language_info": {
-                "codemirror_mode": {
-                    "name": "ipython",
-                    "version": 3
-                },
-                "file_extension": ".py",
-                "mimetype": "text/x-python",
-                "name": "python",
-                "nbconvert_exporter": "python",
-                "pygments_lexer": "ipython3",
-                "version": "3.6.4"
-            }
-        };
-
-        let metadataR = {
-            "kernelspec": {
-                "display_name": "R",
-                "language": "R",
-                "name": "ir"
-            },
-            "language_info": {
-                "codemirror_mode": "r",
-                "file_extension": ".r",
-                "mimetype": "text/x-r-source",
-                "name": "R",
-                "pygments_lexer": "r",
-                "version": "3.5.0"
-            }
-        };
-
-        switch (extName) {
-            case 'py':
-                jupyterFileData["metadata"] = metadataPy; 
-                break;
-            case 'r':
-                jupyterFileData["metadata"] = metadataR;
-                break;
-            default:
-                jupyterFileData["metadata"] = metadataPy;
-                break;
+        jupyterFileDataR = {
+            "nbformat": 4,
+            "nbformat_minor": 2
         }
 
-        jupyterFileData["cells"] = this.cardsExecuted.map(el => el.jupyterData);
+        jupyterFileDataPython["metadata"] = this.metadataPy;
+        jupyterFileDataR["metadata"] = this.metadataR;
 
-        fs.writeFile(fileName, JSON.stringify(jupyterFileData), err => {
-            if (err) console.log(err);
-            console.log("Saved as: " + fileName);
-            vscode.window.showInformationMessage(`Saved as: ${fileName}`);
-            // Also, pop out information box to show that it has been saved
-        });
+        let pyJupyterData: Array<JSONObject> = [], rJupyterData: Array<JSONObject> = [];
+        this.cardsExecuted
+            .map(el => {
+                if(el.kernel === 'python3'){
+                    pyJupyterData.push(el.jupyterData as JSONObject);
+                }
+                else if(el.kernel === 'r'){
+                    rJupyterData.push(el.jupyterData as JSONObject);
+                }
+            });
+
+        jupyterFileDataPython["cells"] = pyJupyterData;
+        jupyterFileDataR["cells"] = rJupyterData;
+
+        this.writeToFile(jupyterFileDataPython, 'python3');
+        this.writeToFile(jupyterFileDataR, 'r');
 
         // fire an event to onExportToJupyter
         this._onExportToJupyter.fire();
     }
-
-
-
 }
 
 
