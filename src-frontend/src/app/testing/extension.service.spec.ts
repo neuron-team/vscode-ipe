6d95149e7b3537191650388c62c1a28eeb85e1b0
jovanhan2 <jovanhan2@gmail.com>
2018-05-28 22:04:00 +0100

    Test: Extension service - window post message corrupting other tests, fixed

--- src-frontend/src/app/testing/extension.service.spec.ts
+++ src-frontend/src/app/testing/extension.service.spec.ts
@@ -1,27 +1,17 @@
 import { ExtensionService } from '../classes/extension.service'
 import { Card, CardOutput } from 'vscode-ipe-types';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { windowWhen } from 'rxjs/operators';
 
 describe('ExtensionService', () => {
     let service = new ExtensionService();
-    const sampleCard = new Card(0, 'sample card', 'print("Hello, World!")', [new CardOutput('stdout', 'Hello, world!')]);
-    
-  it('ExtensionService inteprets add-card command and emits correct card', () => {
-    service.onAddCard.subscribe( p => {
-        expect(p).toEqual({ id: 0, title: 'sample card', sourceCode: 'print("Hello, World!")', outputs: [ Object({ type: 'stdout', output: 'Hello, world!' }) ], collapsed: false, codeCollapsed: true, outputCollapsed: false }, 'expect sample card emit from extension service')
-    })
-    
-    window.postMessage({
-        command: 'add-card',
-        card: sampleCard
-    }, "*")
-  });
+    const sampleCard = new Card(0, 'sample card', 'print("Hello, World!")', [new CardOutput('stdout', 'Hello, world!')], {},'');
 
   it('ExtensionService inteprets non add-card command and does not emit card', () => {
     let tmp = {};
     service.onAddCard.subscribe( p => {
         tmp = p;
     })
-    
     window.postMessage({
         command: 'else',
         card: sampleCard
