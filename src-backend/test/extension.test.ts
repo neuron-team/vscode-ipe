1db2a58eee9c07eb535cc6c9fc2c5b0d1949264c
jovanhan2 <jovanhan2@gmail.com>
2018-05-31 14:29:32 +0100

    Setup mocha testing for backend and added initial test for cardmanager

--- src-backend/test/extension.test.ts
+++ src-backend/test/extension.test.ts
@@ -5,18 +5,25 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
+import {CardManager} from "../cardManager"
+import { Card } from 'vscode-ipe-types';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
-
+describe("CardManager Tests", function () {
+    let cardManager:CardManager;
+    beforeEach(function(done) {
+        cardManager = new CardManager();
+        done();
+    });
     // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    it("Adding card to cards array works", function() {
+        let card = new Card(0,"Hello","source",[],{},'');
+        cardManager.addCard(card);
+        // To access private member data in unit test
+        assert.equal(cardManager["cards"].length,1);  
     });
 });
