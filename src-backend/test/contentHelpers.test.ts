4813bf6585b35ba1fee7341d275135a4156a39ff
Louis Kueh <jovanhan2@gmail.com>
2018-06-03 18:50:54 +0100

    Test: ContentHelper - Added sinon to mock vscode API

--- src-backend/test/contentHelpers.test.ts
+++ src-backend/test/contentHelpers.test.ts
@@ -1,22 +1,20 @@
 //
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
 
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 import {ContentHelpers} from "../contentHelpers"
 import { Card } from 'vscode-ipe-types';
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+var sinon = require('sinon');
+let vscode = require('vscode');
 
 describe("ContentHelper Tests", function () {
-    beforeEach(function(done) {
-        done();
-    });
-    // Defines a Mocha unit test
+ 
+    // beforeEach(function(done) {
+    //     done();
+    // });
+    // afterEach(function() {
+
+    //   });
+
     it("Make title from source code", function() {
         let result = ContentHelpers.makeCardTitle("testing");
         assert.equal(result.toString(),"testing");
@@ -28,5 +26,12 @@ describe("ContentHelper Tests", function () {
     it("Make title from source code with multiple brackets", function() {
         let result = ContentHelpers.makeCardTitle("testing()()\n");
         assert.equal(result.toString(),"testing()");
+    });
+
+    it("Install module calls terminal correctly", function() {
+
+        var mock = sinon.mock(vscode.window);
+        ContentHelpers.installMissingModule("help");
+        mock.expects("createTerminal").once();
     });
 });
