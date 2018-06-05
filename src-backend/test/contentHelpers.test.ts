84b7befeae5f23722a8f7ad2b487bb3c2ea3f66b
jovanhan2 <jovanhan2@gmail.com>
2018-06-05 11:36:20 +0100

    Test: ContentHelper - Added more tests for interpret rich function

--- src-backend/test/contentHelpers.test.ts
+++ src-backend/test/contentHelpers.test.ts
@@ -48,4 +48,38 @@ describe("ContentHelper Tests", function () {
         assert.equal(result.output,testObj.output);
     });
 
+    it("Checking interpret rich function text/plain", function() {
+        let result = ContentHelpers.interpretRich({'text/plain': "hello"});
+        let testObj = new CardOutput("text/plain", "hello");
+        assert.equal(result.type,testObj.type);
+        assert.equal(result.output,testObj.output);
+    });
+
+    it("Checking interpret rich function vnd.jupyter", function() {
+        let result = ContentHelpers.interpretRich({'application/vnd.jupyter': "test"});
+        let testObj = new CardOutput("application/vnd.jupyter", "test");
+        assert.equal(result.type,testObj.type);
+        assert.equal(result.output,testObj.output);
+    });
+    it("Checking interpret rich function vnd.jupyter", function() {
+        let result = ContentHelpers.interpretRich({'application/vnd.jupyter': "test"});
+        let testObj = new CardOutput("application/vnd.jupyter", "test");
+        assert.equal(result.type,testObj.type);
+        assert.equal(result.output,testObj.output);
+    });
+
+    it("Checking interpret rich function image/jepg", function() {
+        let result = ContentHelpers.interpretRich({'image/jpeg': "test"});
+        let testObj = new CardOutput("image/jpeg", "test");
+        assert.equal(result.type,testObj.type);
+        assert.equal(result.output,testObj.output);
+    });
+
+    it("Checking interpret rich function application/vnd.plotly.v1+json", function() {
+        let result = ContentHelpers.interpretRich({'application/vnd.plotly.v1+json': "test"});
+        let testObj = new CardOutput("application/vnd.plotly.v1+json", "test");
+        assert.equal(result.type,testObj.type);
+        assert.equal(result.output,testObj.output);
+    });
+
 });
