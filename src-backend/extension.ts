d9f54765450ffcacd22adef892ab0a87184e5d9d
ml7715 <marclav96@gmail.com>
2018-06-20 21:45:48 +0100

    extension documented

--- src-backend/extension.ts
+++ src-backend/extension.ts
@@ -11,15 +11,40 @@ import { CardManager } from './CardManager';
 import { JSONObject } from '@phosphor/coreutils';
 import {ContentHelpers} from './contentHelpers';
 
-export function activate(context: vscode.ExtensionContext) {    
+export function activate(context: vscode.ExtensionContext) {
+    /**
+     * Webview controller used to generate the output pane.     
+    */
     let webview: WebviewController = new WebviewController(context);
+
+    /**
+     * Interpreter instance, allows code execution.
+     */
     let interpreter = new Interpreter();
+
+    /**
+     * Manages the interaction between VSCode ui elements and the user.
+     */
     let userInteraction: UserInteraction = new UserInteraction(context);
     
+    /**
+     * Boolean which keeps track of the state of the output pane.
+     */
     let panelInitialised: boolean = false;
+
+    /**
+     * Boolean, true when Jupyter Notebook is initialised from
+     * within jupyterManager in the current workspace.
+     */
     let localJupyter: boolean = false;
 
+    /**
+     * Manages the cards in the backend and allows import from and export to
+     * Jupyter Notebook files (.pynb).
+     */
     let cardManager: CardManager = new CardManager();
+
+    // Link webview events to the corresponding cardManager functions
     webview.onMoveCardUp(index => cardManager.moveCardUp(index));
     webview.onMoveCardDown(index => cardManager.moveCardDown(index));
     webview.onDeleteCard(index => cardManager.deleteCard(index));
@@ -33,34 +58,47 @@ export function activate(context: vscode.ExtensionContext) {
     webview.onJupyterExport(indexes => cardManager.exportToJupyter(indexes));
     webview.onSavePdf(pdf => UserInteraction.savePdf(pdf));
     webview.onOpenInBrowser(index => {
+        // Export card to a .pynb file
         let cardId = cardManager.getCardId(index);
         let fileName = 'exportedCardTmp_' + cardId + '.ipynb';
         cardManager.exportToJupyter([index], fileName);
         if (localJupyter) {
+            // If the jupyter has been initialised in the current workspace, open file directly
             interpreter.openNotebookInBrowser(fileName);
         } else {
+            // Otherwise tell the user to select the file created manually
             vscode.window.showInformationMessage('Please open ' + fileName + ' in the Jupyter window');
             interpreter.openNotebookInBrowser(null);
         }
     });
     webview.undoClicked(() => {
+        // Restore the deleted cards if the undo button is pressed by the user
         cardManager.lastDeletedCards.map(card => {
+            // Cards are restored both in the backend and frontend
             cardManager.addCard(card);
             webview.addCard(card);
         })
         cardManager.lastDeletedCards = [];
     });
 
+    // Link cardManager open in notebook event to the interpreter function which provides the functionality
     cardManager.onOpenNotebook(fileName => interpreter.openNotebookInBrowser(fileName));
 
+    // Whenever the status of Jupyter Notebook changes, the status bar is updated
+    // Jupyter: idle || Jupyter: busy
     ContentHelpers.onStatusChanged(status => {
         userInteraction.updateStatus(`Jupyter: ${status}`);
     });
     ContentHelpers.onCardReady((card: Card) => {
+        // Add new cards to both the frontend and the backend
         cardManager.addCard(card);
         webview.addCard(card);
     });
 
+    /**
+     * Initialise the output pane and set up the interpreter.
+     * @param param0    interface containing the base url and the token of the current Jupyter Notebook.
+     */
     function initialisePanel({baseUrl, token}) {
         // Connect to the server defined
         interpreter.connectToServer(baseUrl, token);
@@ -80,18 +118,24 @@ export function activate(context: vscode.ExtensionContext) {
 
         panelInitialised = true;
 
+        // If the kernel is python3, look for module imports
         if (UserInteraction.determineKernel() === 'python3') {
             interpreter.autoImportModules();
         }
     }
 
+    /**
+     * Define callback function of show output pane button.
+     */
     userInteraction.onShowPane(() => {
         if (!panelInitialised) {
-
+            // Check if Jupyter Notebook is installed on the local machine
             if (!JupyterManager.isJupyterInPath()) {
+                // Ask the user to install Jupyter Notebook if not present
                 vscode.window.showInformationMessage('The IPE extension requires Jupyter to be installed. Install now?', 'Install')
                     .then(data => JupyterManager.installJupyter(data));
             } else {
+                // Create a Jupyter Notebook instance automatically
                 let jupyterManager = new JupyterManager();
                 jupyterManager.getJupyterAddressAndToken()
                     .then(info => {
@@ -105,13 +149,20 @@ export function activate(context: vscode.ExtensionContext) {
         
         } else {
             webview.show();
+            // Reset the state of the backend card array when the output pane is closed and opened again
             cardManager.resetState();
         }
     });
 
+    /**
+     * Define callback function called when a full setup
+     * of Jupyter Notebook is requested by the user.
+     */
     userInteraction.onFullSetup(() => {
         if (!panelInitialised) {
+            // Check if Jupyter Manager is installed on the local machine
             if (!JupyterManager.isJupyterInPath()) {
+                // Ask the user to install Jupyter Manager if not present
                 vscode.window.showInformationMessage('The IPE extension requires Jupyter to be installed. Install now?', 'Install')
                     .then(data => JupyterManager.installJupyter(data));
             } else {
@@ -120,6 +171,12 @@ export function activate(context: vscode.ExtensionContext) {
                 runningNotebooks.map(input => {
                     choices.push(input.url);
                 });
+                /**
+                 * Propose different options to the user:
+                 * - Create a new Jupyter Notebook automatically.
+                 * - Enter the infos of an existing Jupyter Notebook instance manually.
+                 * - Choose an existing session of Jupyter Notebook running on the current machine.
+                 */
                 vscode.window.showQuickPick(choices).then(choice => {
                     if (choice === 'Create a new notebook') {
                         let jupyterManager = new JupyterManager();
@@ -132,7 +189,6 @@ export function activate(context: vscode.ExtensionContext) {
                             })
                             .catch(() => vscode.window.showErrorMessage('Could not start a notebook automatically'));
                     } else if (choice === 'Enter details manually') {
-                        vscode.window.showErrorMessage('Could not create a Jupyter instance, enter the server details manually');
                         UserInteraction.askJupyterInfo().then(initialisePanel);
                     } else if(choice) {
                         initialisePanel(runningNotebooks.filter(input => input.url === choice)[0].info);
@@ -142,6 +198,10 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    /**
+     * Define callback function called whenever the
+     * user requests a kernel restart.
+     */
     userInteraction.onRestartKernels(() => {
         if (panelInitialised) {
             interpreter.restartKernels();
@@ -150,6 +210,10 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    /**
+    * Define callback function called when the
+    * user wants to import a notebook.
+    */
     userInteraction.onImportNotebook(() => {
         if(panelInitialised){
             let options: vscode.OpenDialogOptions = { canSelectMany : true, filters : { 'Jupyter Notebook': ['ipynb'] } };
@@ -167,6 +231,10 @@ export function activate(context: vscode.ExtensionContext) {
 
     });
 
+    /**
+     * Determine the right kernel for code execution when
+     * the file being edited by the user changes.
+     */
     vscode.window.onDidChangeActiveTextEditor(input => {
         if (panelInitialised) {
             // Open new kernel if new file is in a different language
@@ -179,6 +247,10 @@ export function activate(context: vscode.ExtensionContext) {
     });
 }
 
+/**
+ * Called when the extension is closed.
+ * Disposes the running instance of Jupyter Notebook.
+ */
 export function deactivate(context: vscode.ExtensionContext) {
     JupyterManager.disposeNotebook();
 }
