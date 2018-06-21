5016f0be4fd67ee3db34fe8b0c683fc10fba289f
jovanhan2 <jovanhan2@gmail.com>
2018-06-21 10:16:11 +0100

    Documented regex service

--- src-frontend/src/app/classes/regex.service.ts
+++ src-frontend/src/app/classes/regex.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class RegexService {
+    /**
+     * Checks if user inputs a regex expression
+     * Defined as anything enclosed between /.../flags
+     * @param search    User inputted string in search box
+     * @param returns   A regex expression object with appropriate flags
+     */
   regexQuery(search: string): RegExp {
     //If there is match between slash
     if (/\/([\s\S]*?)\//.test(search)) {
-        //Get expression between slashes and get regex flags
+    //Get expression between slashes and get regex flags
       return new RegExp(/\/([\s\S]*?)\//.exec(search)[0].slice(1, -1), /[^/]*$/.exec(search)[0])
     }
     return null;
